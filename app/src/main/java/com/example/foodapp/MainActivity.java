5040a8d558d2e815d78e976a2509d54373eebd12
owntrolf <16764256+owntrolf@users.noreply.github.com>
2020-12-02 19:44:16 -0800

    added check for mainactivity

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +24,16 @@ public class MainActivity extends AppCompatActivity {
         mods.ingredients = ((TextView)this.findViewById(R.id.editTextIngred)).getText().toString();
         mods.NiR = ((TextView)this.findViewById(R.id.editTextNiR)).getText().toString();
 
-        Intent intent = new Intent(this, SearchActivity.class);
-        intent.putExtra("ingred", mods.ingredients);
-        intent.putExtra("NiR", mods.NiR);
-        this.startActivityForResult(intent, 0);
+        if(TextUtils.isEmpty(mods.ingredients)) {
+            Toast.makeText(this, "Need to enter an ingredient", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent intent = new Intent(this, SearchActivity.class);
+            intent.putExtra("ingred", mods.ingredients);
+            intent.putExtra("NiR", mods.NiR);
+            this.startActivityForResult(intent, 0);
+        }
+
+
     }
 }
